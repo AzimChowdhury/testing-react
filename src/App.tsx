d717fd5fe36b1f30751ddedc4ec65ad3f3d394a1
Azim Chowdhury <azimchy994@gmail.com>
2023-11-09 23:52:07 +0600

    user

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 
 import './App.css'
 import PrimaryButton from './components/PrimaryButton'
+import Users from './components/Users/User'
 
 function App() {
 
   return (
     <div>
       <h1>Hello World</h1>
-      <PrimaryButton action='Add' />
-      <PrimaryButton action='Delete' />
+      {/* <PrimaryButton action='Add' />
+      <PrimaryButton action='Delete' /> */}
+      <Users />
     </div>
   )
 }
---
+++ src/components/Users/user.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import Users from "./User";
+
+
+it('should render the component', () => {
+    render(<Users />)
+
+    const element = screen.getByRole('textbox', {
+        name: 'Name'
+    })
+
+    const element2 = screen.getByTestId('paragraph')
+
+
+    const headingELement = screen.getByRole('heading', {
+        level: 2
+    })
+    const button = screen.queryByRole('button', {
+        name: 'Submit'
+    })
+
+    expect(button).not.toBeInTheDocument()
+
+    expect(element2).toBeInTheDocument()
+    expect(element).toBeInTheDocument()
+    expect(headingELement).toBeInTheDocument()
+
+})
