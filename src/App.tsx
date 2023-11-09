075c249839b5ef96edecbfdd9026ddfb269dd22d
Azim Chowdhury <azimchy994@gmail.com>
2023-11-10 00:01:18 +0600

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-
 import './App.css'
+import FruitList from './components/FruitList/FruitList';
 import PrimaryButton from './components/PrimaryButton'
 import Users from './components/Users/User'
+
+const fruits = ['apple', 'banana', 'orange', 'mango', 'pineapple'];
+export const users = [
+  { id: 1, name: 'John', age: 32 },
+  { id: 2, name: 'Jane', age: 25 },
+  { id: 3, name: 'Bob', age: 43 },
+];
+
 
 function App() {
 
@@ -11,6 +19,7 @@ function App() {
       {/* <PrimaryButton action='Add' />
       <PrimaryButton action='Delete' /> */}
       <Users />
+      <FruitList fruits={fruits} />
     </div>
   )
 }
---
+++ src/components/FruitList/FruitList.tsx
@@ -0,0 +1,16 @@
+
+
+const FruitList = ({ fruits }: { fruits: string[] }) => {
+    return (
+        <div>
+            <p>Fruit List</p>
+            <ul>
+                {fruits.map((fruit, i) => (
+                    <li key={i}> {fruit}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default FruitList;
